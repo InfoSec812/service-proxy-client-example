9fc815a61481d88f19f7401f4abf704bc6ed3b02
Deven Phillips <deven.phillips@redhat.com>
2017-08-01 10:11:26 -0400

    Added comments to code

--- src/main/resources/webroot/scripts/app.js
+++ src/main/resources/webroot/scripts/app.js
@@ -1,6 +1,10 @@
+// Create an instance of the EventBus
 var eb = new this.EventBus(window.location.origin+"/eventbus/");
+
+// Create an instance of the Service Proxy, but it cannot be used until the EventBus is connected
 var svc = new TestService(eb, "test.service");
 
+// Create a function handler for service results
 var resHandler = function(err, res) {
     var newChild = document.createElement('div');
     if (err === undefined || err === null) {
@@ -12,12 +16,14 @@ var resHandler = function(err, res) {
     svcCaller();
 };
 
+// Create a timer loop to call the service every 3 seconds
 var svcCaller = function() {
     window.setTimeout(function() {
         svc.test(resHandler);
     }, 3000);
 };
 
+// Once the EventBus is connected, start the timer loop.
 eb.onopen = function() {
     svcCaller();
 }
