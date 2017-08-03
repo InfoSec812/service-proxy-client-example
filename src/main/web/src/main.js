8b3fe8e25dfc597c417b3fce71ddfc74d8a75888
Deven Phillips <deven.phillips@redhat.com>
2017-08-02 21:37:58 -0400

    Integrate service proxy

--- src/main/web/src/main.js
+++ src/main/web/src/main.js
@@ -1,4 +1,5 @@
 import EventBus from 'vertx3-eventbus-client';
+import TestService from '@/lib/test-service-js/test_service-proxy';
 import Vue from 'vue';
 
 new Vue({
@@ -7,28 +8,45 @@ new Vue({
         PERIODIC_TIMER: 'periodic.timer',
         eb: null,
         messages: [],
-        ebErrCount: 0
+        ebErrCount: 0,
+        testSvc: null
     },
     methods: {
+        pushMessage(msg) {
+            this.messages.push(msg);
+            if (this.messages.length>20) {
+                this.messages.shift();
+            }
+        },
         reconnect() {
             this.eb = new EventBus(window.location.origin+'/eventbus/');
             this.eb.onopen = () => {
                 this.ebErrCount = 0;
                 console.log('EventBus bridge initialized');
+
+                // Initialize the service proxy
+                this.testSvc = new TestService(this.eb, "test.service");
+
+                // Register a handler for incoming messages off of the EventBus
                 this.eb.registerHandler(this.PERIODIC_TIMER, (err, res) => {
                     if (err) {
-                        this.messages.push(JSON.stringify(err));
+                        this.pushMessage(JSON.stringify(err));
                     } else {
-                        this.messages.push(res.body);
-                    }
-                    if (this.messages.length>20) {
-                        this.messages.shift();
+                        if (res.body==='SPACER') {
+                            // When the incoming message is 'SPACER', make a call
+                            // using the Service Proxy and push the result.
+                            this.testSvc.test((err1, res1) => {
+                                if (err1) {
+                                    this.pushMessage(JSON.stringify(err1));
+                                } else {
+                                    this.pushMessage(res1);
+                                }
+                            });
+                        } else {
+                            this.pushMessage(res.body);
+                        }
                     }
                 });
-            };
-            this.eb.onclose = () => {
-                this.ebErrCount++;
-                setTimeout(this.reconnect, 500*this.ebErrCount);
             };
         }
     },
